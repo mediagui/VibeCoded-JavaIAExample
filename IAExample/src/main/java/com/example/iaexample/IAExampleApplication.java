cf84d145a9f8520b78cc90d97e5469f2f90071de
Miguel Ángel Medina <mediagui@gmail.com>
2026-02-22 21:00:07 +0100

    feat: add Spring Boot main application class

---
+++ IAExample/src/main/java/com/example/iaexample/IAExampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.iaexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IAExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IAExampleApplication.class, args);
+    }
+
+}
